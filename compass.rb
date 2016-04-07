88c885f6561cddc55b7ba3128546a880c14f301b
kidatsy <toshiro.kida@gmail.com>
2016-04-07 12:28:29 -0400

    initial commit

---
+++ compass.rb
@@ -0,0 +1,18 @@
+http_path = "./"
+css_dir = "dist/assets/crisistextline/styles"
+sass_dir = "src/assets/crisistextline/sass"
+# images_dir = "public/img"
+# javascripts_dir = "public/js"
+
+# path to source files
+project_path = "./"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
